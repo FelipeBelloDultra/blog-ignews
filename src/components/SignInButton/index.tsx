f157240ea8a386aef75152a8723b563d205fd54e
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2021-07-18 01:38:17 -0300

    feat(Auth): Create auth with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,25 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  console.log(session);
+
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub
         color="#04d361"
       />
 
-      Felipe Bello Dultra
+      {session.user?.name}
 
       <FiX
         color="#737380"
@@ -26,6 +30,7 @@ export function SignInButton() {
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub
         color="#eba417"
